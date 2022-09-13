15bbe58a067872f47a75845d326e3ad3c5a448bd
bearbig12 <bearbig23@gmail.com>
2022-09-13 13:35:25 +0100

    PlayerMoveController
    Move forward, jump, left and right Update

---
+++ GameJamProject/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController controller;
+    private float mPlayerSpeed = 10.0f;
+    private Vector3 HeadingVector;
+    private float JumpVelocity = 0.0f;
+    private float mGravity = 19.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HeadingVector = Vector3.zero;
+
+
+        // Jump
+        if(controller.isGrounded)
+        {
+            JumpVelocity = -0.5f;
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+                JumpVelocity = mPlayerSpeed * 1.5f;
+            }
+        }
+        else
+        {
+            JumpVelocity -= mGravity * Time.deltaTime;
+        }
+        HeadingVector.y = JumpVelocity;
+
+        // Left & Right
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            HeadingVector.x -= mPlayerSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            HeadingVector.x += mPlayerSpeed;
+        }
+        
+
+
+        // Forward
+        HeadingVector.z = mPlayerSpeed;
+
+        controller.Move(HeadingVector * Time.deltaTime);
+    }
+}
